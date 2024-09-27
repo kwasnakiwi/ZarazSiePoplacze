25e3152d9654c88bd7a80cebc83adde3b98e31ea
kwasnakiwi <jakzab199@gmail.com>
2024-09-27 20:33:24 +0200

    added smth

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -23,18 +23,24 @@ function Navbar() {
             <li className='navbar2-line hideOnMobile'><a>KONTAKT</a></li>
             <li className='navbar2-line hideOnMobile'><a><h3 className='navbar2-h3'>tel.421 325 326<br/>
             spas@xd.com</h3></a></li>
-            <li className='navbar2-line'><a onClick={function(){
-              const navbar3 = document.querySelector('.navbar3')
-              navbar3.style.display = 'flex'
-            }}>E</a></li>
+            <li className='navbar2-line'><a onClick={function showNavbar(): void {
+  const navbar3 = document.querySelector('.navbar3') as HTMLElement;
+  if (navbar3) {
+    navbar3.style.display = 'flex';
+  }
+}
+}>E</a></li>
           </ul>
         </div>
         <div className="navbar3">
           <ul className='navbar3-list'>
-            <li className='navbar3-line'><a onClick={function(){
-              const navbar3 = document.querySelector('.navbar3')
-              navbar3.style.display = 'none'
-            }}>X</a></li>
+            <li className='navbar3-line'><a onClick={function hideNavbar(): void {
+  const navbar3 = document.querySelector('.navbar3') as HTMLElement;
+  if (navbar3) {
+    navbar3.style.display = 'none';
+  }
+}
+}>X</a></li>
             <li className='navbar3-line'><a><img src={pizzalogo}/></a></li>
             <li className='navbar3-line '><a>MENU</a></li>
             <li className='navbar3-line '><a>O NAS</a></li>
